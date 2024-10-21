82ae07109c8060a57ec6f65ea591cc5090030f79
unknown <andreandson09@gmail.comgit config --global user.name Andson>
2024-10-21 17:04:28 -0300

    Initial commit

--- Procfile
+++
@@ -1,8 +0,0 @@
-web: python app.py
-
-
-
-
-
-
-
--- app.py
+++ app.py
@@ -159,5 +159,6 @@ def update_table(fabricantes, anos, paises, tipos):
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
 
